98fd442349df061655e96474bb4ace7cdff0e021
usman358 <usman_khalid35@yahoo.com>
2022-08-03 11:11:53 +0500

    first commit

---
+++ myapp/src/data.js
@@ -0,0 +1,39 @@
+export const data=
+[
+    {
+    packagName:"WEEKLY SOCIAL PLUS",
+    data : "8GB",
+    description:"(whatsapp,imo,viber,youtube)",
+    mins: "500 jazz&10 other network minutes",
+    price: "200(incl.tax)",
+    },
+
+     {
+    packagName:"MONTHLY SUPER DUPER",
+    data : "12GB",
+    //description:"(whatsapp,imo,viber,youtube)",
+    mins: "5000 jazz&100 other network minutes",
+    price: "800(incl.tax)",
+    },
+
+     {
+    packagName:"WEEKLY PLUS",
+    data : "5GB",
+    description:"(12AM-1PM)",
+    mins: "1000 jazz&100 other network minutes",
+    price: "300(incl.tax)",
+    },
+
+     {
+    packagName:"WEEKLY DUPER PLUS",
+    data : "8GB",
+    description:"(whatsapp,imo,viber,youtube)",
+    mins: "500 jazz&10 other network minutes",
+    price: "300(incl.tax)",
+    };
+
+
+
+
+
+]
